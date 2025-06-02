f7b8ddbe3c2a89e6005ac4f545e21a7fbf6908f9
Talha Can Havadar <havadartalha@gmail.com>
2025-06-03 00:10:07 +0200

    cli: add tests for command parser (#3)

--- src/cli.zig
+++ src/cli.zig
@@ -3,19 +3,15 @@ const Allocator = @import("std").mem.Allocator;
 const Command = @import("cli/command.zig").Command;
 const CommandError = @import("cli/command.zig").Error;
 
-fn parse(alloc: Allocator) !Command {
+fn parse(args: [][:0]u8) !Command {
     //_ = alloc;
     const my_commands = @typeInfo(Command).@"union".fields;
-    const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
 
     if (args.len < 2) {
         return CommandError.UnknownCommand;
     }
-    std.debug.print("{any}\n", .{@TypeOf(args[1])});
     inline for (my_commands) |c| {
         if (std.mem.eql(u8, args[1], c.name)) {
-            std.debug.print("{s}\n", .{c.name});
             return @field(Command, c.name);
         }
     }
@@ -27,7 +23,31 @@ pub fn run() !void {
     defer std.debug.assert(gpa.deinit() == .ok);
 
     const allocator = gpa.allocator();
-    const command = try parse(allocator);
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const command = try parse(args);
     const return_code = try command.run(allocator);
     std.debug.print("return_code: {d}\n", .{return_code});
+}
+
+test "parse check command" {
+    const expectEqual = std.testing.expectEqual;
+    const args: [2][:0]const u8 = .{ "sparse", "check" };
+    const command = try parse(@constCast(@ptrCast(&args)));
+    try expectEqual("check", @tagName(command));
+}
+
+test "parse new command" {
+    const expectEqual = std.testing.expectEqual;
+    const args: [2][:0]const u8 = .{ "sparse", "new" };
+    const command = try parse(@constCast(@ptrCast(&args)));
+    try expectEqual("new", @tagName(command));
+}
+
+test "parse a non existent command" {
+    const expectEqual = std.testing.expectEqual;
+    const args: [2][:0]const u8 = .{ "sparse", "boo" };
+    const command = parse(@constCast(@ptrCast(&args))) catch |e| e;
+    try expectEqual(CommandError.UnknownCommand, command);
 }

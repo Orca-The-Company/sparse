9d7eb26282cd272d628b81a13a594fa8ce97eebf
Talha Can Havadar <havadartalha@gmail.com>
2025-06-08 21:33:40 +0200

    libgit2: implement GitReferenceIterator with glob functionality

--- src/lib/libgit2/libgit2.zig
+++ src/lib/libgit2/libgit2.zig
@@ -2,6 +2,7 @@ const c = @import("c.zig").c;
 pub const GitError = @import("error.zig").GitError;
 pub const GitRepository = @import("repository.zig").GitRepository;
 pub const GitReference = @import("reference.zig").GitReference;
+pub const GitReferenceIterator = @import("reference.zig").GitReferenceIterator;
 
 pub fn init() !void {
     const res: c_int = c.git_libgit2_init();

426aaef3c59cdb9e67733360c9ec41811c82acd8
Talha Can Havadar <havadartalha@gmail.com>
2025-06-17 00:16:42 +0200

    cli: handle functions in options

--- src/cli/feature_command.zig
+++ src/cli/feature_command.zig
@@ -1,13 +1,19 @@
 const std = @import("std");
 const Allocator = @import("std").mem.Allocator;
 const log = @import("std").log.scoped(.feature_command);
-//const FeatureCommand = @This();
 
 const Params = struct {
     feature_name: [1][]u8,
     slice_name: ?[1][]u8 = null,
     _options: struct {
+        const Options = @This();
         @"--to": []const u8 = "main",
+        @"--help": *const fn () void = Options.help,
+        @"-h": *const fn () void = Options.help,
+
+        pub fn help() void {
+            std.debug.print("this is an help text\n", .{});
+        }
     } = .{},
 };
 pub const FeatureCommand = struct {
@@ -27,12 +33,16 @@ pub const FeatureCommand = struct {
         defer std.process.argsFree(alloc, args);
         log.debug("got cli arguments: {s}", .{args});
 
-        const cli_positionals = try command.parseOptions(
+        const cli_positionals = command.parseOptions(
             @TypeOf(params._options),
             alloc,
             &params._options,
             args,
-        );
+        ) catch |err| switch (err) {
+            command.Error.OptionHandledAlready => return 0,
+            else => return err,
+        };
+
         defer alloc.free(cli_positionals);
         try command.parsePositionals(
             Params,

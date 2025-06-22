e2f5cda037770eaf29d96a62021a00f9d36c53b5
Talha Can Havadar <havadartalha@gmail.com>
2025-06-22 19:14:51 +0200

    sparse: detect leaf slices properly
    fixed memory management issues regarding GitRepository instances and
    refactored the leaf slice detection logic so now we are ableto detect
    which branch is created from which one. That way we have more robust way
    of detecting the leaf and this also gives as flexibility when we want to
    create slices in between slices.

--- src/lib/slice.zig
+++ src/lib/slice.zig
@@ -125,10 +125,7 @@ pub const Slice = struct {
     }) ![]Slice {
         log.debug("getAllSlicesWith::", .{});
         const repo = try GitRepository.open();
-        // No need to free repo here since we are returning slices with refs
-        // TODO: we have a nasty memory leak here that we need to refactor
-        // how we handle repo for references
-        // defer repo.free();
+        defer repo.free();
 
         const sparse_ref_prefix = try utils.sparseBranchRefPrefix(.{
             .alloc = o.alloc,

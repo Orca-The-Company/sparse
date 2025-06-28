063bec5b731dfc0f3a54c2c224b8b0d2e0d0d3a0
Talha Can Havadar <havadartalha@gmail.com>
2025-06-28 19:54:37 +0200

    sparse: update all slices when no conflict
    This commit enables `sparse update` to work properly when there is no
    conflict detected during rebase. So for conflict cases to be handled
    properly I put some TODOs for future myself.
    
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- src/lib/sparse.zig
+++ src/lib/sparse.zig
@@ -6,6 +6,8 @@ pub const Error = error{
     BACKEND_UNABLE_TO_GET_REFS,
     UNABLE_TO_SWITCH_BRANCHES,
     CORRUPTED_FEATURE,
+    REPARENTING_FAILED,
+    REBASE_IN_PROGRESS,
     UNABLE_TO_DETECT_CURRENT_FEATURE,
     RECOVERABLE_ORPHAN_SLICES_IN_FEATURE,
     RECOVERABLE_FORKED_SLICES_IN_FEATURE,
@@ -189,31 +191,131 @@ pub fn slice(o: struct { slice_name: ?[]const u8 }) !void {
 /// target, ensuring a clean and consistent state within the feature.
 pub fn update(o: struct {
     alloc: std.mem.Allocator,
+    @"continue": bool = false,
 }) !void {
     try LibGit.init();
     defer LibGit.shutdown() catch @panic("Oops: couldn't shutdown libgit2, something weird is cooking...");
+    const repo = try LibGit.GitRepository.open();
+    defer repo.free();
 
-    var current_feature = try Feature.activeFeature(.{ .alloc = o.alloc });
-    defer {
-        if (current_feature) |*f| f.free(o.alloc);
-    }
-    if (current_feature) |cf| {
-        const target = try cf.target(o.alloc);
-        log.debug("update:: current_feature.name:{s} current_feature.target:{s} current_feature.ref_name:{s}", .{
-            cf.name,
-            if (target) |t| t.name() else "null",
-            cf.ref_name,
-        });
-        //try Slice.printSliceGraph(std.io.getStdOut().writer(), cf.slices.?.items);
-        const leaves = try Slice.leafNodes(.{ .alloc = o.alloc, .slice_pool = cf.slices.?.items });
-        defer o.alloc.free(leaves);
-        var ss: ?*Slice = leaves[0];
-        while (ss != null) : (ss = ss.?.target) {
-            _ = try ss.?.isMerged(.{ .alloc = o.alloc, .into = try target.?.upstream(ss.?.repo) });
-        }
+    // Check if a rebase is in progress
+    const is_rebase_in_progress = try Git.isRebaseInProgress(o.alloc, repo);
+    if (is_rebase_in_progress) {
+        log.err("update:: rebase in progress", .{});
+        // TODO: print an informative message about the rebase in progress and
+        // suggest user to fix the conflicts and run `git rebase --continue`
+        // once it is done run `sparse update --continue`
+        return Error.REBASE_IN_PROGRESS;
     } else {
-        log.err("update:: not able to detect current branch", .{});
-        return Error.UNABLE_TO_DETECT_CURRENT_FEATURE;
+        var current_feature = try Feature.activeFeature(.{ .alloc = o.alloc });
+        defer {
+            if (current_feature) |*f| f.free(o.alloc);
+        }
+        if (current_feature) |cf| {
+            const target = try cf.target(o.alloc);
+            log.debug("update:: current_feature.name:{s} current_feature.target:{s} current_feature.ref_name:{s}", .{
+                cf.name,
+                if (target) |t| t.name() else "null",
+                cf.ref_name,
+            });
+            const leaves = try Slice.leafNodes(.{
+                .alloc = o.alloc,
+                .slice_pool = cf.slices.?.items,
+            });
+            defer o.alloc.free(leaves);
+            var ss: ?*Slice = leaves[0];
+            const upstream = try target.?.upstream(ss.?.repo);
+            defer upstream.free();
+
+            var last_unmerged: ?*Slice = ss;
+            while (ss != null) : (ss = ss.?.target) {
+                const is_merged = try ss.?.isMerged(.{
+                    .alloc = o.alloc,
+                    .into = upstream,
+                });
+                if (!is_merged) {
+                    last_unmerged = ss;
+                } else break;
+            }
+            if (last_unmerged) |lu| {
+                log.debug("update:: last_unmerged:{s}", .{lu.ref.name()});
+                // re-parent last_unmerged to target
+                // rebase all slices from tip to target
+                // git rebase --onto <new-parent> <old-parent> <branch-to-move>
+                const old_parent = res: {
+                    if (lu.ref.createdFrom(lu.repo)) |op_ref| {
+                        break :res op_ref.name();
+                    } else {
+                        if (target) |t| {
+                            break :res t.name();
+                        } else {
+                            // TODO: Handle the case when target is null
+                            // At least fall back to default target for sparse in
+                            // git config, maybe something like `sparse.default.target`
+                            break :res "main";
+                        }
+                    }
+                };
+
+                // TODO: save state of the sparse update we need this information
+                // in case there is a conflict or error during the rebase process
+                // so that we can resume the update from where it left off
+
+                try reparent(.{
+                    .alloc = o.alloc,
+                    .tip_slice = leaves[0],
+                    .old_parent = old_parent,
+                    .new_parent = target.?,
+                    .branch_to_move = lu.ref,
+                });
+
+                {
+                    const rr_rebase = try Git.rebase(
+                        .{
+                            .allocator = o.alloc,
+                            .args = &.{
+                                lu.ref.name(),
+                                "--update-refs",
+                            },
+                        },
+                    );
+                    defer o.alloc.free(rr_rebase.stdout);
+                    defer o.alloc.free(rr_rebase.stderr);
+                    log.debug("update:: rebase stdout: {s}", .{rr_rebase.stdout});
+                    if (rr_rebase.term.Exited != 0) {
+                        log.debug("update:: rebase stderr: {s}", .{rr_rebase.stderr});
+                        log.err(
+                            "update:: rebase failed with exit code {d}",
+                            .{rr_rebase.term.Exited},
+                        );
+                        return Error.REPARENTING_FAILED;
+                    }
+                }
+
+                // TODO: cleanup merged slices, loop forward since last_unmerged
+                // is the last unmerged slice
+                // var current = lu;
+                // while (current != null) : (current = current.?.target) {
+                //     if (current.?.isMerged(.{
+                //         .alloc = o.alloc,
+                //         .into = try target.?.upstream(current.?.repo),
+                //     })) {
+                //         current.?.cleanupMergedSlices(o.alloc);
+                //     }
+                // }
+
+            } else {
+                log.debug("update:: no unmerged slices", .{});
+            }
+        } else {
+            if (!o.@"continue") {
+                log.err("update:: not able to detect current branch", .{});
+                return Error.UNABLE_TO_DETECT_CURRENT_FEATURE;
+            }
+            // TODO: implement the continue operation
+            // TODO: check if this is a valid continue operation
+            // we need to check there is already a sparse update in progress
+        }
     }
 }
 
@@ -221,6 +323,60 @@ pub fn submit(opts: struct {}) !void {
     _ = opts;
     std.debug.print("\n===sparse-submit===\n\n", .{});
     std.debug.print("\n====================\n", .{});
+}
+
+fn reparent(o: struct {
+    alloc: std.mem.Allocator,
+    tip_slice: *const Slice,
+    new_parent: GitReference,
+    old_parent: []const u8,
+    branch_to_move: GitReference,
+    continuing: bool = false,
+}) !void {
+    if (!o.continuing) {
+        const rr_rebase = try Git.rebase(.{
+            .allocator = o.alloc,
+            .args = &[_][]const u8{
+                "-r",
+                "--onto",
+                o.new_parent.name(),
+                o.old_parent,
+                o.branch_to_move.name(),
+                "--update-refs",
+            },
+        });
+        defer o.alloc.free(rr_rebase.stderr);
+        defer o.alloc.free(rr_rebase.stdout);
+        log.debug("update:: rebase stdout: {s}", .{rr_rebase.stdout});
+        if (rr_rebase.term.Exited != 0) {
+            log.debug("update:: rebase stderr: {s}", .{rr_rebase.stderr});
+            log.err(
+                "update:: rebase failed during re-parenting with exit code {d}",
+                .{rr_rebase.term.Exited},
+            );
+            // TODO: let user know what to do next
+            return Error.REPARENTING_FAILED;
+        }
+    }
+    // do switching to tip no matter what
+    {
+        const rr_switch = try Git.@"switch"(.{
+            .allocator = o.alloc,
+            .args = &.{
+                try o.tip_slice.ref.branchName(),
+            },
+        });
+        defer o.alloc.free(rr_switch.stdout);
+        defer o.alloc.free(rr_switch.stderr);
+        if (rr_switch.term.Exited != 0) {
+            log.debug("update:: switch stderr: {s}", .{rr_switch.stderr});
+            log.err(
+                "update:: switch failed with exit code {d}",
+                .{rr_switch.term.Exited},
+            );
+            return Error.REPARENTING_FAILED;
+        }
+    }
 }
 
 fn jump(o: struct {
@@ -287,6 +443,7 @@ const constants = @import("constants.zig");
 const LibGit = @import("libgit2/libgit2.zig");
 const GitString = LibGit.GitString;
 const GitBranch = LibGit.GitBranch;
+const GitReference = LibGit.GitReference;
 const GitBranchType = LibGit.GitBranchType;
 const Git = @import("system/Git.zig");
 const Feature = @import("Feature.zig");

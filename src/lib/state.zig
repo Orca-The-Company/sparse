046ef5951b3fc2c07ffd24cf2434b97c23201266
Talha Can Havadar <havadartalha@gmail.com>
2025-06-30 17:29:42 +0200

    sparse: use dirs to load/save Update state
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- src/lib/state.zig
+++ src/lib/state.zig
@@ -1,5 +1,6 @@
 //! Module that manages the state of the sparse commands
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const State = @This();
 const state_dir_name = "sparse";
 
@@ -9,12 +10,20 @@ pub const Error = error{
 
 pub const Update = struct {
     const file_name = "update-state";
-    const UpdateData = struct {
-        feature: []const u8,
-        target: []const u8,
-        last_unmerged_slice: []const u8,
+    const Operation = enum {
+        Created,
+        Analyzed,
+        Reparented,
+        Completed,
+        Failed,
     };
-    data: UpdateData,
+    const UpdateData = struct {
+        feature: ?[]const u8,
+        target: ?[]const u8,
+        last_unmerged_slice: ?[]const u8,
+        last_operation: Operation,
+    };
+    _data: UpdateData,
     _repo: GitRepository,
 
     /// Indicates whether the update is in progress
@@ -25,36 +34,69 @@ pub const Update = struct {
         // we can use .git/sparse/update-state file to determine if
         // an update is in progress
         @panic("Not implemented");
-        return false;
     }
 
     pub fn save(self: Update) !void {
-        _ = self;
-        @panic("Not implemented");
+        var state_dir = try Update.stateDir(self._repo);
+        defer state_dir.close();
+
+        const file = try state_dir.createFile(file_name, .{});
+        defer file.close();
+        try std.zon.stringify.serialize(self._data, .{}, file.writer());
     }
 
-    pub fn load(alloc: std.mem.Allocator, repo: GitRepository) !Update {
-        const update_state_file = try std.fs.path.join(alloc, &.{
-            repo.commondir(),
-            state_dir_name,
+    pub fn load(alloc: Allocator, repo: GitRepository) !Update {
+        var state_dir = try Update.stateDir(repo);
+        defer state_dir.close();
+        const file = state_dir.openFile(
             file_name,
-        });
-        const file = try std.fs.openFileAbsolute(
-            update_state_file,
             .{ .mode = .read_only },
-        );
+        ) catch |err| {
+            if (err == error.FileNotFound) {
+                return Update{
+                    ._repo = repo,
+                    ._data = UpdateData{
+                        .feature = null,
+                        .target = null,
+                        .last_unmerged_slice = null,
+                        .last_operation = .Created,
+                    },
+                };
+            }
+            return err;
+        };
+        defer file.close();
         const content = try file.readToEndAlloc(alloc, 4096);
+        defer alloc.free(content);
         const data = try std.zon.parse.fromSlice(
             UpdateData,
             alloc,
-            content,
+            @ptrCast(content),
             null,
             .{},
         );
         return .{
             ._repo = repo,
-            .data = data,
+            ._data = data,
         };
+    }
+
+    pub fn free(self: Update, alloc: Allocator) void {
+        if (self._data.feature) |feature| {
+            alloc.free(feature);
+        }
+        if (self._data.target) |target| {
+            alloc.free(target);
+        }
+        if (self._data.last_unmerged_slice) |slice| {
+            alloc.free(slice);
+        }
+    }
+
+    fn stateDir(repo: GitRepository) !std.fs.Dir {
+        var git_dir = try std.fs.openDirAbsolute(repo.commondir(), .{});
+        defer git_dir.close();
+        return try git_dir.makeOpenPath(state_dir_name, .{});
     }
 };
 

ae742241f6c91bc3fa266d8a1664ab4ab8e31cbc
Talha Can Havadar <havadartalha@gmail.com>
2025-07-27 17:33:23 +0200

    libgit2: zig fmt
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- src/lib/libgit2/signature.zig
+++ src/lib/libgit2/signature.zig
@@ -7,39 +7,39 @@ pub const GitSignature = struct {
     // Creates signature using git config (user.name and user.email)
     pub fn default(repo: GitRepository) !GitSignature {
         var signature: ?*c.git_signature = null;
-        
+
         const res = c.git_signature_default(&signature, repo.value);
-        
+
         if (res < 0) {
             return GitError.SIGNATURE_DEFAULT_FAILED;
         }
-        
+
         return GitSignature{ .value = signature };
     }
 
     // Creates signature with current timestamp
     pub fn now(name: []const u8, email: []const u8) !GitSignature {
         var signature: ?*c.git_signature = null;
-        
+
         const res = c.git_signature_now(&signature, name.ptr, email.ptr);
-        
+
         if (res < 0) {
             return GitError.SIGNATURE_CREATE_FAILED;
         }
-        
+
         return GitSignature{ .value = signature };
     }
 
     // Creates signature with explicit timestamp
     pub fn new(name: []const u8, email: []const u8, time: i64, offset: c_int) !GitSignature {
         var signature: ?*c.git_signature = null;
-        
+
         const res = c.git_signature_new(&signature, name.ptr, email.ptr, time, offset);
-        
+
         if (res < 0) {
             return GitError.SIGNATURE_CREATE_FAILED;
         }
-        
+
         return GitSignature{ .value = signature };
     }
 

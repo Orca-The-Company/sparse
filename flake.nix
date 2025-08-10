8a7cecd9bacbd1d026b7aecb1e6ab7305d7d5689
Talha Can Havadar <havadartalha@gmail.com>
2025-08-10 14:37:18 +0200

    nix: add package support with patch-based build modifications
    - Add packages output to flake.nix alongside existing devShells
    - Create patch file for build system compatibility with Nix sandboxed builds
    - Use proper git format-patch generated patches instead of sed commands
    - Successfully builds sparse binary with all functionality intact

--- flake.nix
+++ flake.nix
@@ -22,6 +22,57 @@
         zig-nixpkgs = inputs.zig-nixpkgs.legacyPackages.${system};
       in
       {
+        packages = rec {
+          default = sparse;
+          
+          sparse = zig-nixpkgs.stdenv.mkDerivation rec {
+            pname = "sparse";
+            version = "0.0.0";
+            
+            src = zig-nixpkgs.lib.cleanSource ./.;
+            
+            patches = [
+              ./nix/patches/0001-nix-build-compatibility.patch
+            ];
+            
+            nativeBuildInputs = [
+              zig-nixpkgs.zig
+              zig-nixpkgs.pkg-config
+            ];
+            
+            buildInputs = [
+              zig-nixpkgs.openssl
+            ] ++ zig-nixpkgs.lib.optionals zig-nixpkgs.stdenv.isDarwin [
+              zig-nixpkgs.darwin.apple_sdk.frameworks.Security
+              zig-nixpkgs.darwin.apple_sdk.frameworks.CoreFoundation
+              zig-nixpkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+              zig-nixpkgs.darwin.libiconv
+            ];
+            
+            dontConfigure = true;
+            
+            buildPhase = ''
+              runHook preBuild
+              export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+              mkdir -p $ZIG_GLOBAL_CACHE_DIR
+              ${zig-nixpkgs.lib.optionalString zig-nixpkgs.stdenv.isDarwin ''
+                export NIX_LDFLAGS="-F${zig-nixpkgs.darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation -L${zig-nixpkgs.darwin.libiconv}/lib $NIX_LDFLAGS"
+              ''}
+              zig build --prefix $out -Doptimize=ReleaseSafe
+              runHook postBuild
+            '';
+            
+            meta = {
+              description = "A Zig-based CLI tool for stacked pull request workflows";
+              homepage = "https://github.com/Orca-The-Company/sparse";
+              license = zig-nixpkgs.lib.licenses.mit;
+              maintainers = [];
+              platforms = zig-nixpkgs.lib.platforms.unix;
+              mainProgram = "sparse";
+            };
+          };
+        };
+
         devShells.default = zig-nixpkgs.mkShell {
           packages = [
             zig-nixpkgs.zig
